6e9e0a83096560c34b162bc9c58182e342bf5b06
OGUZHAN DURAN <oguzhnduran@gmail.com>
2022-02-11 10:09:39 +0300

    İcon was Added

---
+++ Yapılanlar.js
@@ -0,0 +1,2 @@
+// Önce React Bootstrap'i yüklüyoruz. ( yarn add bootstrap react-bootstrap )
+// import 'bootstrap/dist/css/bootstrap.min.css'; 'i en src dizininin altındaki index.js dosyasına ekle.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Selam</h1>
+      <img
+        className="w-8 dark:bg-gray-600 rounded-full"
+        src={require(`./icons/clear-day.svg`)}
+        alt="sun"
+        title="Light Mode"
+      />
     </div>
   );
 }
